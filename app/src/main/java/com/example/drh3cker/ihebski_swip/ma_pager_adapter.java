1051706b3b5278103be9a454e3818e6603a42ceb
adarshbhatt <adarsh56@gmail.com>
2015-06-07 01:24:38 -0700

    Initial commit

---
+++ app/src/main/java/com/example/drh3cker/ihebski_swip/ma_pager_adapter.java
@@ -0,0 +1,64 @@
+package com.example.drh3cker.ihebski_swip;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+/**
+ * Created by Dr.h3cker on 14/03/2015.
+ */
+public class ma_pager_adapter extends FragmentPagerAdapter {
+    public ma_pager_adapter(FragmentManager fm) {
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int i) {
+
+        switch (i) {
+            case 0:
+                tab1 t1 = new tab1();
+                return t1;
+            case 1:
+                tab2 t2 = new tab2();
+                return t2;
+            case 2:
+                tab3 t3 = new tab3();
+                return t3;
+            case 3:
+                tab4 t4 = new tab4();
+                return t4;
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }//set the number of tabs
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return "jing";
+            case 1:
+
+                return "ping";
+            case 2:
+
+                return "selmon";
+            case 3:
+                return "bingu";
+        }
+        return null;
+    }
+
+
+
+}
